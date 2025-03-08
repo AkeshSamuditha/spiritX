0a7faa80ad1548e29e37714088537c7cf88ee201
Akesh Samuditha <akesh.20@cse.mrt.ac.lk>
2025-03-08 13:02:38 +0530

    trial

---
+++ spiritx-p1/app/signup/page.jsx
@@ -0,0 +1,9 @@
+import { SignupForm } from "@/lib/ui/signup-form";
+
+export default function SignupPage() {
+    return (
+        <div className="flex h-screen w-screen items-center justify-center">
+            <SignupForm />
+        </div>
+    );
+}
